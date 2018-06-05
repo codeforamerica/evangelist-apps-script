6240dec6416ae9d01852cf736faf482f4ff86406
Tom Dooner <tdooner@codeforamerica.org>
2018-06-05 15:42:40 -0700

    Update Slack form updater to use new javascript

--- src/SlackSignupForm.js
+++ src/SlackSignupForm.js
@@ -1,36 +1,34 @@
+const {
+  SHEET_NAMES,
+} = require('./Code.js');
+
 const SLACK_SIGNUP_FORM_ID = '17BXzqiA_cYAfpDSILHDnlavQOXV8kHgsYWp4f8ayUt4';
 
 module.exports = {
   slackSignupForm() {
     const form = FormApp.openById(SLACK_SIGNUP_FORM_ID);
 
-    var brigadeListItem = form.getItems(FormApp.ItemType.LIST).filter(i => i.getTitle().indexOf('which Brigade do you attend?') !== -1);
+    let brigadeListItem = form
+      .getItems(FormApp.ItemType.LIST)
+      .filter(i => i.getTitle().indexOf('which Brigade do you attend?') !== -1);
 
     if (brigadeListItem.length !== 1) {
-      Logger.log('ERROR: Could not find brigade question in signup form');
-      return;
+      throw new Error('ERROR: Could not find brigade Slack question in signup form');
     }
 
-    var brigadeListItem = brigadeListItem[0].asListItem();
+    brigadeListItem = brigadeListItem[0].asListItem();
 
     // get list of brigades from salesforce sheet
-    const salesforce = SpreadsheetApp.getActive().getSheetByName(SHEET_NAMES.salesforce);
-    const salesforceHeaders = salesforce.getRange(1, 1, 1, salesforce.getLastColumn()).getValues()[0];
-    const salesforceContents = salesforce.getRange(2, 1, salesforce.getLastRow(), salesforce.getLastColumn())
-      .getValues();
+    const salesforceContents = SpreadsheetApp.getActive().getSheetByName(SHEET_NAMES.salesforce)
+      .getDataRange().getValues();
+    const salesforceHeaders = salesforceContents.shift();
+    const headerActiveIndex = salesforceHeaders.indexOf('Active?');
+    const headerNameIndex = salesforceHeaders.indexOf('Name');
 
-    const brigadeListChoices = [];
-    for (const i in salesforceContents) {
-      const brigade = salesforceContents[i];
-      const isActive = brigade[salesforceHeaders.indexOf('Active?')];
-
-      if (!isActive) {
-        continue;
-      }
-
-      brigadeListChoices.push(brigadeListItem.createChoice(brigade[salesforceHeaders.indexOf('Name')]));
-    }
+    const brigadeListChoices = salesforceContents
+      .filter(row => row[headerActiveIndex])
+      .map(row => brigadeListItem.createChoice(row[headerNameIndex]));
 
     brigadeListItem.setChoices(brigadeListChoices);
-  }
+  },
 };
