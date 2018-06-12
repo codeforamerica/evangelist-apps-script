a6d494c29018ffa7294b9af3b471f7685b42ec2a
Tom Dooner <tdooner@codeforamerica.org>
2018-06-12 13:57:33 -0700

    Fix eslint errors in src/SendEmail.js

--- src/SendEmail.js
+++ src/SendEmail.js
@@ -1,9 +1,24 @@
+const { SHEET_NAMES } = require('./Code.js');
+const {
+  loadAll,
+} = require('./Code.js');
+const {
+  compareDatabaseAndSalesforce,
+  databaseTestBrigadeURLs,
+} = require('./BrigadeDatabase.js');
+
+// eslint-di
 function sendEmail() {
   loadAll();
   compareDatabaseAndSalesforce();
   databaseTestBrigadeURLs();
 
-  const contents = SpreadsheetApp.getActive().getSheetByName(SHEET_NAMES.todo).getDataRange().getValues();
+  const contents = SpreadsheetApp
+    .getActive()
+    .getSheetByName(SHEET_NAMES.todo)
+    .getDataRange()
+    .getValues();
+
   const idxMissingSalesforce = contents[0].indexOf('Missing from Salesforce');
   const idxMissingBrigadeInfo = contents[0].indexOf('Missing from brigade-information');
   const idxMissingPrimaryContact = contents[0].indexOf('Missing Primary Contact');
@@ -13,43 +28,43 @@ function sendEmail() {
   contents.shift(); // remove header row
 
   let body = '<p>Missing from Salesforce:</p><ul>';
-  for (var i in contents) {
-    if (contents[i][idxMissingSalesforce] && contents[i][idxMissingSalesforce].length) {
-      body += `<li>${contents[i][idxMissingSalesforce]}</li>`;
+  contents.forEach((row) => {
+    if (row[idxMissingSalesforce] && row[idxMissingSalesforce].length) {
+      body += `<li>${row[idxMissingSalesforce]}</li>`;
     }
-  }
+  });
   body += '</ul>';
 
   body += '<p>Missing from brigade-information repo:</p><ul>';
-  for (var i in contents) {
-    if (contents[i][idxMissingBrigadeInfo] && contents[i][idxMissingBrigadeInfo].length) {
-      body += `<li>${contents[i][idxMissingBrigadeInfo]}</li>`;
+  contents.forEach((row) => {
+    if (row[idxMissingBrigadeInfo] && row[idxMissingBrigadeInfo].length) {
+      body += `<li>${row[idxMissingBrigadeInfo]}</li>`;
     }
-  }
+  });
   body += '</ul>';
 
   body += '<p>Missing Primary Contact in Salesforce:</p><ul>';
-  for (var i in contents) {
-    if (contents[i][idxMissingPrimaryContact] && contents[i][idxMissingPrimaryContact].length) {
-      body += `<li>${contents[i][idxMissingPrimaryContact]}</li>`;
+  contents.forEach((row) => {
+    if (row[idxMissingPrimaryContact] && row[idxMissingPrimaryContact].length) {
+      body += `<li>${row[idxMissingPrimaryContact]}</li>`;
     }
-  }
+  });
   body += '</ul>';
 
   body += '<p>Primary Contact needs added to brigadeleads@:</p><ul>';
-  for (var i in contents) {
-    if (contents[i][idxAddBrigadeLeads] && contents[i][idxAddBrigadeLeads].length) {
-      body += `<li>${contents[i][idxAddBrigadeLeads]}</li>`;
+  contents.forEach((row) => {
+    if (row[idxAddBrigadeLeads] && row[idxAddBrigadeLeads].length) {
+      body += `<li>${row[idxAddBrigadeLeads]}</li>`;
     }
-  }
+  });
   body += '</ul>';
 
   body += '<p>Missing Meetup User ID in Salesforce:</p><ul>';
-  for (var i in contents) {
-    if (contents[i][idxMissingMeetupUserId] && contents[i][idxMissingMeetupUserId].length) {
-      body += `<li>${contents[i][idxMissingMeetupUserId]}</li>`;
+  contents.forEach((row) => {
+    if (row[idxMissingMeetupUserId] && row[idxMissingMeetupUserId].length) {
+      body += `<li>${row[idxMissingMeetupUserId]}</li>`;
     }
-  }
+  });
 
   body += `<p>Other Log Output:</p><pre>${Logger.getLog()}</pre>`;
 
@@ -59,3 +74,7 @@ function sendEmail() {
     htmlBody: body,
   });
 }
+
+module.exports = {
+  sendEmail,
+};
