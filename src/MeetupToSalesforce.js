9ff3fd5faec2738d3233cab030dd259ef9dda5a7
Tom Dooner <tdooner@codeforamerica.org>
2018-06-14 09:42:08 -0700

    Add contactsToAdd / contactsToUpsert distinction
    We're doing a two phase process for Contacts:
    
    1. Attempt to create all contacts that should exist. Salesforce will
    reject any duplicates.
    
    2. Upsert all contacts to update the fields we want to update. (We don't
    want to update their name if it already exists from another source.)
    
    And then continue with the other objects.

--- src/MeetupToSalesforce.js
+++ src/MeetupToSalesforce.js
@@ -1,4 +1,5 @@
-const fullNameSplitter = require('full-name-splitter');
+// TODO: figure out why "default" is required here
+const fullNameSplitter = require('full-name-splitter').default;
 
 const {
   convertMeetupTime,
@@ -12,26 +13,38 @@ const {
 } = require('./Salesforce.js');
 
 /*
-* TODO:
-* - Populate it, and then use it in the import.
-*/
+ * The staging sheet is meant to give a bit of visibility into the process and
+ * also allow for CSV export.
+ */
 const SALESFORCE_STAGING_SHEET_ID = '1bmSgDPBB5buJUBsYvtUND87lHEdOhDAVCq41uFdHzdM';
 
 SHEET_NAMES.meetupToSalesforceOrganizations = '[AUTO] Meetup Organizations';
 SHEET_NAMES.meetupToSalesforceAffiliations = '[AUTO] Existing Affiliations from Salesforce';
-SHEET_NAMES.meetupToSalesforceContactsToCreate = 'Contacts to Create';
-SHEET_NAMES.meetupToSalesforceAffiliationsToUpdate = 'Affiliations to Update';
+SHEET_NAMES.meetupToSalesforceContactsToCreate = '1. Contacts to Create';
+SHEET_NAMES.meetupToSalesforceContactsToUpsert = '2. Contacts to Upsert';
+SHEET_NAMES.meetupToSalesforceAffiliationsToUpdate = '3. Affiliations to Upsert';
+
+const TMP_MEETUP_USER_ID_FILTER = id => parseInt(id, 10) === 104952772;
 
 /*
  * Populates the "Contacts to Create" / "Affiliations to Create"
+ *
+ * TODO:
+ *   Load brigade name -> salesforce ID map
+ *   Wire it up on the Affiliations to Upsert page
+ *   Actually make sure that any loaded Affiliations to upsert have the existing ID
+ *
  */
 const MEETUP_TO_SALESFORCE_CONTACTS_HEADERS = [
   'Meetup_User_ID__c', 'FirstName', 'LastName', 'Email', 'MC_Brigade_Newsletter__c', 'Program_Interest_Brigade__c',
 ];
+const MEETUP_TO_SALESFORCE_CONTACTS_UPSERT_HEADERS = [
+  'Email', 'Meetup_User_ID__c', 'MC_Brigade_Newsletter__c', 'Program_Interest_Brigade__c',
+];
 const MEETUP_TO_SALESFORCE_AFFILIATIONS_TO_UPDATE_HEADERS = [
   'npe5__Contact__r:Meetup_User_ID__c', 'npe5__Organization__c', 'npe5__StartDate__c', 'npe5__EndDate__c', 'Source__c',
 ];
-const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+const THREE_MONTHS_IN_MS = 3 * 30 * 24 * 60 * 60 * 1000;
 function meetupToSalesforceLoadRecordsToCreateAndUpdate() {
   const meetupMembers = SpreadsheetApp.openById(MEETUP_MEMBERSHIP_SPREADSHEET_ID)
     .getSheetByName(SHEET_NAMES.meetupMembers)
@@ -39,46 +52,47 @@ function meetupToSalesforceLoadRecordsToCreateAndUpdate() {
     .getValues();
 
   const meetupMembersHeaders = meetupMembers.shift();
-  const contacts = [];
+  const contactsToAdd = [];
+  const contactsToUpsert = [];
   const affiliations = [];
-  meetupMembers.each((meetupMember) => {
-    // don't import people without an email address
-    const meetupMemberEmail = meetupMember[meetupMembersHeaders.indexOf('Email Address')];
-    if (!meetupMemberEmail || !meetupMemberEmail.length) {
-      return;
-    }
 
-    // don't import people who have never attended an event
-    const meetupMemberEvents = parseInt(meetupMember[meetupMembersHeaders.indexOf('Events Attended')], 10);
-    if (meetupMemberEvents === 0) {
-      return;
-    }
+  meetupMembers
+    // only sync members with email addresses
+    .filter(member => member[meetupMembersHeaders.indexOf('Email Address')])
+    // temporarily filter out almost everyone for testing
+    .filter(member => TMP_MEETUP_USER_ID_FILTER(member[meetupMembersHeaders.indexOf('Meetup ID')]))
+    .forEach((member) => {
+      const guessedFirstAndLastName = fullNameSplitter(member[meetupMembersHeaders.indexOf('Full Name')]);
 
-    // add the person to the contacts array
-    const guessedFirstAndLastName = fullNameSplitter(meetupMember[meetupMembersHeaders.indexOf('Full Name')]);
-    contacts.push([
-      meetupMember[meetupMembersHeaders.indexOf('Meetup ID')],
-      guessedFirstAndLastName[0],
-      guessedFirstAndLastName[1],
-      meetupMember[meetupMembersHeaders.indexOf('Email Address')],
-      'TRUE',
-      'TRUE',
-    ]);
-
-    // then, add an affiliation for every brigade
-    const meetupMemberBrigades = JSON.parse(meetupMember[meetupMembersHeaders.indexOf('Chapters')]);
-    meetupMemberBrigades.forEach((brigade) => {
-      affiliations.push([
-        meetupMember[meetupMembersHeaders.indexOf('Meetup ID')],
-        `TODO: ${brigade.name}`,
-        convertMeetupTime(meetupMember[meetupMembersHeaders.indexOf('Join Time')]),
-        convertMeetupTime(meetupMember[meetupMembersHeaders.indexOf('Join Time')] + THREE_DAYS_IN_MS),
-        'Meetup',
+      contactsToAdd.push([
+        member[meetupMembersHeaders.indexOf('Meetup ID')],
+        guessedFirstAndLastName[0],
+        guessedFirstAndLastName[1],
+        member[meetupMembersHeaders.indexOf('Email Address')],
+        'TRUE',
+        'TRUE',
       ]);
-    });
-  });
 
-  console.log(`Dumping ${contacts.length} contacts to sheet`);
+      contactsToUpsert.push([
+        member[meetupMembersHeaders.indexOf('Email Address')],
+        member[meetupMembersHeaders.indexOf('Meetup ID')],
+        'TRUE',
+        'TRUE',
+      ]);
+
+      const meetupMemberBrigades = JSON.parse(member[meetupMembersHeaders.indexOf('Chapters')]);
+      meetupMemberBrigades.forEach((brigade) => {
+        affiliations.push([
+          member[meetupMembersHeaders.indexOf('Meetup ID')],
+          `TODO: ${brigade.name}`,
+          convertMeetupTime(member[meetupMembersHeaders.indexOf('Join Time')]),
+          convertMeetupTime(member[meetupMembersHeaders.indexOf('Last Access Time')] + THREE_MONTHS_IN_MS),
+          'Meetup',
+        ]);
+      });
+    });
+
+  console.log(`Dumping ${contactsToAdd.length} contacts to add`);
   console.log(`Dumping ${affiliations.length} affiliations to sheet`);
 
   // finally, dump everything to the sheet
@@ -89,8 +103,18 @@ function meetupToSalesforceLoadRecordsToCreateAndUpdate() {
     .setValues([MEETUP_TO_SALESFORCE_CONTACTS_HEADERS])
     .setFontWeight('bold')
     .getSheet()
-    .getRange(2, 1, contacts.length, contacts[0].length)
-    .setValues(contacts)
+    .getRange(2, 1, contactsToAdd.length, contactsToAdd[0].length)
+    .setValues(contactsToAdd)
+    .getSheet()
+    .getParent()
+    .getSheetByName(SHEET_NAMES.meetupToSalesforceContactsToUpsert)
+    .clear()
+    .getRange(1, 1, 1, MEETUP_TO_SALESFORCE_CONTACTS_UPSERT_HEADERS.length)
+    .setValues([MEETUP_TO_SALESFORCE_CONTACTS_UPSERT_HEADERS])
+    .setFontWeight('bold')
+    .getSheet()
+    .getRange(2, 1, contactsToUpsert.length, contactsToUpsert[0].length)
+    .setValues(contactsToUpsert)
     .getSheet()
     .getParent()
     .getSheetByName(SHEET_NAMES.meetupToSalesforceAffiliationsToUpdate)
