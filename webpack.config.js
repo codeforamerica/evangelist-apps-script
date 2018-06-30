33fe798f6756fd8433cf56f8dd3ef45a139a5520
Tom Dooner <tdooner@codeforamerica.org>
2018-06-29 17:01:07 -0700

    Add comment about node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,8 @@ module.exports = {
   module: {
     rules: [
       {
+        // NOTE: we intentionally DO NOT exclude `node_modules` from processing
+        // with babel. See the commit 5550905 for details.
         test: /\.js$/,
         use: {
           loader: 'babel-loader',
