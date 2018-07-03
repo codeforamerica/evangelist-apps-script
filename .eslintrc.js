cf9c5c76be5ae71463902690305e33b0c0c0de89
Tom Dooner <tdooner@codeforamerica.org>
2018-07-03 11:49:37 -0700

    Add eslint-plugin-jasmine
    This informs eslint about all the jasmine globals as well as adding a
    few jasmine-specific lints.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   "extends": "airbnb-base",
   "plugins": [
-    "googleappsscript"
+    "googleappsscript",
+    "jasmine",
   ],
   "env": {
     "googleappsscript/googleappsscript": true,
+    "jasmine": true,
   },
   "rules": {
     "no-console": 0, // console.log is our best logging method
